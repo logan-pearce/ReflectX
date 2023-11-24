1806b26401b669e62ae30beae5f26ea005b72ac1
Logan Pearce <loganpearce55@gmail.com>
2023-11-24 13:49:16 -0700

    update conf

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -29,8 +29,9 @@ templates_path = ['_templates']
 
 # -- Options for HTML output
 
-extensions = [ "sphinx_rtd_theme", ]
-html_theme = "sphinx_rtd_theme"
+extensions = ["sphinx_rtd_theme"]
+#html_theme = "sphinx_rtd_theme"
+html_theme = "classic"
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
